0829090fb26ca529d3b01709ae845b7d27703fed
Kucherenko Dmytro <dimakucherenko91@gmail.com>
2024-09-18 18:46:26 +0300

    Added related name

--- taxi/models.py
+++ taxi/models.py
@@ -14,7 +14,7 @@ class Manufacturer(models.Model):
 
 class Car(models.Model):
     model = models.CharField(max_length=300)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
 
 
